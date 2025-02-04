4ed2552ec89edf03a790119c661cbf9e779a79b5
adamaroussi67+1@gmail.com <adam.AROUSSI-BENTATA@etu.univ-amu.fr>
2025-02-04 15:06:26 +0100

    creation de la class Livre et son test

---
+++ src/bibliotheque/livre.java
@@ -0,0 +1,41 @@
+package bibliotheque;
+
+public class livre {
+		private String name;
+		private Float price;
+		private int quantite;
+		
+		
+		
+		public livre() {
+		}
+		
+		// constructeur
+		public livre (String name, Float price, int quantite) {
+			this.name= name;
+			this.price = price;
+			this.quantite = quantite;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public Float getPrice() {
+			return price;
+		}
+
+		public int getQuantite() {
+			return quantite;
+		}
+		
+		public String ToString() {
+			return "Livre{" +
+		               "name='" + name + '\'' +
+		               ", price=" + price +
+		               ", quantite=" + quantite +
+		               '}';
+		}
+
+}      
+
---
+++ src/bibliotheque/testLivre.java
@@ -0,0 +1,10 @@
+package bibliotheque;
+
+public class testLivre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
