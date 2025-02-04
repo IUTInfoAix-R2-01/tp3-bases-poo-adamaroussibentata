3c2d637b68ad112ea87de47addabb238ded14306
adamaroussi67+1@gmail.com <adam.AROUSSI-BENTATA@etu.univ-amu.fr>
2025-02-04 17:11:49 +0100

    exo invoiceItem creation du script basic

---
+++ src/bibliotheque/InvoiceItem.java
@@ -0,0 +1,45 @@
+package bibliotheque;
+
+public class InvoiceItem {
+	private String id;
+	private String desc;
+	private int qty;
+	private double unitPrice;
+	
+	public InvoiceItem () {
+		
+	}
+	
+	public InvoiceItem (String id, String desc, int qty, double unitPrice) {
+		this.id = id;
+		this.desc = desc;
+		this.qty = qty;
+		this.unitPrice = unitPrice;
+		
+	public String getId() {
+		return id;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+	
+	public int getQty{
+		return qty;
+	}
+	public double getUnitPrice{
+		return unitPrice;
+	}
+	
+	public String toString(){
+		return "id" +
+	               "desc='" + desc + '\'' +
+	               ", qty=" + qty +
+	               ", unitPrice=" + unitPrice +
+	               '}';
+	}
+	
+	
+	
+
+}
